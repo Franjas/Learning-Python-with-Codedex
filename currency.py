c504ac91b6faca40f4178e7bd94dbd22aee1a416
franjas <franjas3@gmail.com>
2022-10-18 14:25:51 -0400

    9 - Flip of a coin

--- currency.py
+++ currency.py
@@ -5,6 +5,6 @@ cny = float(input('Enter the amount in CNY: '))
 jpy = float(input('Enter the amount in JPY: '))
 krw = float(input('Enter the amount in KRW: '))
 
-amount = (cny * 0.14) + (jpy * 0.0067) + (krw * 0.00070)  # exchange rate 18/10/2022
+amount = (cny * 0.14) + (jpy * 0.0067) + (krw * 0.00070) # exchange rate 18/10/2022
 
 print('The amount in USD is:', '$',amount)
