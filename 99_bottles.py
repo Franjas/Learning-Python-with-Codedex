94a516c67dba8df9d14113b96199cbb35d175374
franjas <franjas3@gmail.com>
2022-10-25 13:00:24 -0400

    18 - 99 bottles of beer on the wall

---
+++ 99_bottles.py
@@ -0,0 +1,5 @@
+# Create a program and use a for loop and a range() function to print out all the verses of "99 Bottles of Beer".
+# Codédex
+
+for x in range(99, 0, -1):
+    print(f'{x} bottles of beer on the wall, {x} bottles of beer\nTake one down, pass it around, {x -1} bottles of beer on the wall.')
