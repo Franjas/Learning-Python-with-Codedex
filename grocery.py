824b7947dfd8f65acd6cac57195da2ce272f6478
franjas <franjas3@gmail.com>
2022-10-26 14:52:05 -0400

    20 - List-Grocery

---
+++ grocery.py
@@ -0,0 +1,5 @@
+# Create a program that with a grocery list of things that you need to get from the store.
+# Codédex
+
+grocery = ['Eggs','Avocados','Cookies','Hot Pepper jam','Blueberries','Broccoli','Potatos','Bananas']
+print(grocery)
