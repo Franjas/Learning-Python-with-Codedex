557886afa05b256bdefb53828c83b88b78aec34c
franjas <franjas3@gmail.com>
2022-10-28 11:56:10 -0400

    21 - To-Do List

---
+++ todo.py
@@ -0,0 +1,10 @@
+# Create a program that will define a todo list.
+# Print the first item and the second item. What did you get? Remember, indices in Python start from 0.
+# Next, use slicing to print the third, fourth, and fifth items.
+# Try printing out the item at index 9 to see the IndexError before moving on.
+# Codédex
+
+todo = ['Get quarters.','Do laundry.', 'Take a walk.', 'Get a haircut.','Make some tea.', 'Complete List chapter.', 'Call mom.']
+#print(todo[0 : 2])
+#print(todo[2 : 5])
+print(todo[9])
