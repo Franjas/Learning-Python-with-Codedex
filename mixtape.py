05394ef27e595847bf9e9c0c2ca01a863a995b07
franjas <franjas3@gmail.com>
2022-10-31 13:12:20 -0400

    24 - Mixtape

---
+++ mixtape.py
@@ -0,0 +1,25 @@
+# Create a program and think of a theme for the songs.
+# Define a playlist list and throw in some songs.
+# Now loop over the list and print everything out!
+# Try different ways to do so before moving on.
+# Codédex
+
+playlist = ['Porches - Rangerover',
+            'Mount Eerie - You Swan Go On',
+            'Caroline Polachek - Look at Me Now',
+            'Pinegegrove - Darkness',
+            'LVP UP - Spirit Was',
+            'First Love - Late Spring']
+print(playlist)
+#######################################
+playlist.append('Yeah Yeah Yeah - Heads Will Roll')
+print(playlist)
+#######################################
+playlist.insert(4, 'Perfume Genius - Queen')
+print(playlist)
+#######################################
+playlist.reverse()
+print(playlist)
+#######################################
+playlist = playlist.index('LVP UP - Spirit Was')
+print(playlist)
